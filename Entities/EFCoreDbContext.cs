8ca7b9a2b2d240b1bcc71830ca5bc01165dc2323
ja204 <jagajanardhan1@gmail.com>
2024-12-28 17:43:56 +0530

    Add project files.

---
+++ Entities/EFCoreDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+namespace EFCoreCodeFirstDemo5.Entities
+{
+    public class EFCoreDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Configuring the Connection String
+            optionsBuilder.UseSqlServer(@"Server=HP\SQLSERVERDEV;Database=EFCoreDB;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
